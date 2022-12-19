37f47d226838ff42779083922552529c61471324
TRUONG\joritori <nguyentruong4720@gmail.com>
2022-12-19 16:46:45 +0700

    dang thu

---
+++ g/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  for (size_t i = 0; i < count; i++)
+  {
+    /* code */
+  }
+  
+  // put your main code here, to run repeatedly:
+}
